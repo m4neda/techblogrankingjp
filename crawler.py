d649d59b04aad4e094ea4f0a577cbb1627867785
m4neda <m4neda@gmail.com>
2021-01-26 17:45:25 +0900

    add tempporary crawler implementation

--- crawler.py
+++ crawler.py
@@ -3,7 +3,13 @@ import requests
 from bs4 import BeautifulSoup
 import time
 from logging import getLogger
+import urllib.parse as urlparse
+from urllib.parse import parse_qs
+
 logger = getLogger(__name__)
+
+
+NEXT_PAGE_SELECTOR = "[class='entrylist-readmore js-keyboard-selectable-item'] > a"
 
 
 def get_keywords_for_search(file_name: str) -> list[str]:
@@ -12,10 +18,15 @@ def get_keywords_for_search(file_name: str) -> list[str]:
     return keyword_list
 
 
-def get_articles_from_hatebu(url: str) -> BeautifulSoup:
+def get_response(url, page):
     endpoint = 'https://b.hatena.ne.jp/entrylist'
     try:
-        r = requests.get(endpoint, params={'url': url, 'sort': 'eid'})
+        r = requests.get(
+            endpoint,
+            params={
+                'url': url,
+                'page': page,
+                'sort': 'eid'})
     except requests.exceptions.SSLError:
         r = requests.get(
             endpoint,
@@ -24,8 +35,15 @@ def get_articles_from_hatebu(url: str) -> BeautifulSoup:
                 'sort': 'eid'},
             verify=False)
     time.sleep(1)
+    return r
 
-    soup = BeautifulSoup(r.content, 'lxml')
+
+def get_soup(response: requests.models.Response) -> BeautifulSoup:
+    soup = BeautifulSoup(response.content, 'lxml')
+    return soup
+
+
+def get_articles_from_hatebu(soup) -> BeautifulSoup:
     articles = soup.find_all('a',
                              {'class': 'js-keyboard-openable',
                               'data-gtm-click-label': 'entry-info-title',
@@ -35,23 +53,30 @@ def get_articles_from_hatebu(url: str) -> BeautifulSoup:
     if len(articles) > MAX_NUMBER_OF_ARTICLES_IN_ONE_PAGE:
         logger.error(f"length of articles={len(articles)}")
         raise Exception('length of articles was over 29, Something wrong')
-    return soup
+    return articles
 
 
-def save_article_urls(soup: BeautifulSoup):
-    for tag in soup:
-        article_url = tag['href']
-    pass
+def get_all_entrylist(url, entrylist, page_count):
+    response: requests.models.Response = get_response(url, page_count)
+    soup: BeautifulSoup = get_soup(response)
+    entrylist.append(soup)
+    nexts: list = soup.select_one(NEXT_PAGE_SELECTOR)
+    if nexts:
+        next_page_url: str = nexts['href']
+        parsed = urlparse.urlparse(next_page_url)
+        page_count = parse_qs(parsed.query)['page'][0]
+        get_all_entrylist(url, entrylist, page_count)
+    return entrylist
 
 
 def main():
     keywords: List[str] = get_keywords_for_search(
         file_name='hatena_keyword.txt')
-
-    for keyword in keywords:
-        soup: BeautifulSoup = get_articles_from_hatebu(url=keyword)
-
-        save_article_urls(soup)
+    keyword = keywords[0]  # pick one for test
+    entrylist: List[BeautifulSoup] = []
+    entrylist = get_all_entrylist(keyword, entrylist, page_count=None)
+    for entry in entrylist:
+        articles = get_articles_from_hatebu(entry)
 
 
 if __name__ == '__main__':
