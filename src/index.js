de1c7ee7138a66fa219708101fdd22b51383de3d
m4neda <m4neda@gmail.com>
2021-02-17 16:56:18 +0900

    fix linted point,avoid pylint bug with python v3.9

--- src/index.js
+++ src/index.js
@@ -1,28 +1,32 @@
+/* global gridjs */
+
 new gridjs.Grid({
   columns: [
     {
-      name:'順位',
+      name: '順位',
       formatter: (cell) => {
-        return gridjs.html(`<b>${cell}</b>`)}
+        return gridjs.html(`<b>${cell}</b>`)
+      }
     },
     {
-      name:'企業名',
+      name: '企業名',
       formatter: (cell, row) => {
-        return gridjs.html(`<a href=https://b.hatena.ne.jp/entrylist?url=${row.cells[5].data}>${cell}</a>`)}
+        return gridjs.html(`<a href=https://b.hatena.ne.jp/entrylist?url=${row.cells[5].data}>${cell}</a>`)
+      }
     },
     'スコア',
     '記事数',
     'ブクマ数中央値',
     {
-      name:'url',
+      name: 'url',
       hidden: true
     }
   ],
   sort: true,
-  search:true,
+  search: true,
   pagination: true,
   server: {
     url: 'http://localhost:8080/',
-    then: data => data.map(result => [result.rank, result.company_name, result.score,result.article_count, result.hatebu_count, result.url])
+    then: data => data.map(result => [result.rank, result.company_name, result.score, result.article_count, result.hatebu_count, result.url])
   }
-}).render(document.getElementById("wrapper"));
+}).render(document.getElementById('wrapper'))
